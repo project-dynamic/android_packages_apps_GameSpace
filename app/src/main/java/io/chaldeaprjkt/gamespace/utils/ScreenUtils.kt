a013a3bd9a8924fc6ab23039b21df6db3c16ed4e
minaripenguin <minaripenguin@users.noreply.github.com>
2024-05-20 06:54:30 +0800

    Revert "GameSpace: Update lock gesture"
    * immersive lock has been implemented
    
    This reverts commit ddd16158c0cbf34bd36960e44af19110ecd6ca32.
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- app/src/main/java/io/chaldeaprjkt/gamespace/utils/ScreenUtils.kt
+++ app/src/main/java/io/chaldeaprjkt/gamespace/utils/ScreenUtils.kt
@@ -26,13 +26,9 @@ import android.os.Handler
 import android.os.IBinder
 import android.os.Looper
 import android.os.PowerManager
-import android.os.RemoteException
-import android.os.ServiceManager
 import android.os.UserHandle
-import android.util.Log
 import android.provider.Settings
 import android.view.WindowManager
-import com.android.internal.statusbar.IStatusBarService
 import com.android.internal.util.ScreenshotHelper
 import com.android.systemui.screenrecord.IRemoteRecording
 import javax.inject.Inject
@@ -62,11 +58,8 @@ class ScreenUtils @Inject constructor(private val context: Context) {
     }
 
     val recorder: IRemoteRecording? get() = remoteRecording
-    
+
     private var isGestureLocked = false
-    private val statusBarService = IStatusBarService.Stub.asInterface(
-        ServiceManager.getService(Context.STATUS_BAR_SERVICE)
-    )
 
     fun bind() {
         isRecorderBound = context.bindServiceAsUser(Intent().apply {
@@ -89,11 +82,10 @@ class ScreenUtils @Inject constructor(private val context: Context) {
             context.unbindService(recorderConnection)
         }
         remoteRecording = null
-        try {
-            statusBarService.setBlockedGesturalNavigation(false)
+        if (isGestureLocked) {
+            Settings.Secure.putInt(context.contentResolver,
+                    "lock_immersive_sysui", 0)
             isGestureLocked = false
-        } catch (e: RemoteException) {
-            Log.e("GameSpace:ScreenUtils", "Failed to toggle gesture off")
         }
     }
 
@@ -120,12 +112,9 @@ class ScreenUtils @Inject constructor(private val context: Context) {
     var lockGesture = false
         get() = isGestureLocked
         set(enable) {
-            try {
-                statusBarService.setBlockedGesturalNavigation(enable)
-                field = enable
-                isGestureLocked = enable
-            } catch (e: RemoteException) {
-                Log.e("GameSpace:ScreenUtils", "Failed to toggle gesture")
-            }
+            Settings.Secure.putInt(context.contentResolver,
+                    "lock_immersive_sysui", if (enable) 1 else 0)
+            field = enable
+            isGestureLocked = enable
         }
 }
