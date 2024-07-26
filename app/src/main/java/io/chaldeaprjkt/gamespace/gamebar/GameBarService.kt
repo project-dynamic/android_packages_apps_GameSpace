3ce556227e68dceac4881fb20fab42babdf7be1c
Dhina17 <dhinalogu@gmail.com>
2024-07-26 13:58:41 +0530

    GameSpace: Don't add floating action if game closed too quickly
    We use handler.postDelayed() to draw the floating action with
    500ms delay on starting game.
    If user opens and closes the game too quickly,
    the game is getting closed and called onGameLeave()
    before the handler.postDelayed execution so the floating
    action is drawn even after closing the game.
    
    Even the worse, user can't close that floating action without
    reboot and can create infinite floating actions.
    
    So don't draw the floating action if game closed before that.
    
    Change-Id: I7fc3748137ff6d3fdb71b8b3590580963f4d0c96
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- app/src/main/java/io/chaldeaprjkt/gamespace/gamebar/GameBarService.kt
+++ app/src/main/java/io/chaldeaprjkt/gamespace/gamebar/GameBarService.kt
@@ -129,6 +129,9 @@ class GameBarService : Hilt_GameBarService() {
             }
         }
 
+    // Whether to ignore the initActions (floating action) or not
+    private var shouldClose = false
+
     override fun onCreate() {
         super.onCreate()
         val frame = FrameLayout(this)
@@ -176,6 +179,7 @@ class GameBarService : Hilt_GameBarService() {
 
     // for client service
     fun onGameStart() {
+        shouldClose = false
         rootBarView.isVisible = false
         rootBarView.alpha = 0f
         updateRootBarView()
@@ -183,6 +187,7 @@ class GameBarService : Hilt_GameBarService() {
     }
 
     fun onGameLeave() {
+        shouldClose = true
         if (::rootPanelView.isInitialized && rootPanelView.isAttachedToWindow) {
             wm.removeViewImmediate(rootPanelView)
         }
@@ -215,6 +220,7 @@ class GameBarService : Hilt_GameBarService() {
     }
 
     private fun initActions() {
+        if (shouldClose) return
         rootBarView.isVisible = true
         rootBarView.animate()
             .alpha(1f)
